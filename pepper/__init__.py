2b3b33c34dd8650918c8b6f50dc8df6b005b63a2
Dj1312 <18169686+Dj1312@users.noreply.github.com>
2023-03-12 21:46:03 +0100

    beginning of the Tidy3D FDFD parser

--- pepper/__init__.py
+++ pepper/__init__.py
@@ -1,2 +1,2 @@
-#from .simulation import SimulationType, Simulation
 from .simulation import SimulationFdfd_TE, SimulationFdfd_TM
+from .interface_tidy3d import TidySimulationFdfd
---
+++ pepper/interface_tidy3d.py
@@ -0,0 +1,5 @@
+import tidy3d as td
+
+
+class TidySimulationFdfd(td.Simulation):
+    run_time: float = -1  # No runtime required in FDFD
