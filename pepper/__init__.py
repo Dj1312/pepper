9ce107cb7a582028967a80fbf6c759a65c7bf5c8
Dj1312 <18169686+Dj1312@users.noreply.github.com>
2023-03-13 10:30:16 +0100

    fix: add LiNbO3 to material

--- pepper/__init__.py
+++ pepper/__init__.py
@@ -1,3 +1,3 @@
 from .interface_tidy3d import TidySimulationFdfd
-from .material import material_library
+from .material import material_library, material_refs
 from .simulation import SimulationFdfd_TE, SimulationFdfd_TM
--- pepper/material.py
+++ pepper/material.py
@@ -7,9 +7,11 @@ import numpy as np
 # from tidy3d.components.medium import AbstractMedium, DispersiveMedium, ensure_freq_in_range
 # from tidy3d.components.base import Tidy3dBaseModel
 # from tidy3d.constants import C_0, MICROMETER
-from tidy3d.components.medium import PoleResidue
-from tidy3d.material_library.material_library import MaterialItem, VariantItem, material_library
-from tidy3d.material_library.material_reference import material_refs, ReferenceData
+from tidy3d.components.medium import PoleResidue, Sellmeier
+from tidy3d.material_library.material_library import MaterialItem, VariantItem
+from tidy3d.material_library.material_library import material_library as tidy3d_material_library
+from tidy3d.material_library.material_reference import ReferenceData
+from tidy3d.material_library.material_reference import material_refs as tidy3d_material_refs
 
 
 # Impossible to use actually since Tidy3D want a PoleResidue model
@@ -49,29 +51,19 @@ from tidy3d.material_library.material_reference import material_refs, ReferenceD
 #         n = self._n_model(frequency)
 #         return AbstractMedium.nk_to_eps_complex(n)
 
-
-# class LithiumNiobate = td.VariantItem()
-# class VariantItem(Tidy3dBaseModel):
-#     """Reference, data_source, and material model for a variant of a material."""
-
-#     medium: PoleResidue = pd.Field(
-#         ...,
-#         title="Material dispersion model",
-#         description="A dispersive medium described by the pole-residue pair model.",
-#     )
-
-#     reference: List[ReferenceData] = pd.Field(
-#         None,
-#         title="Reference information",
-#         description="A list of reference related to this variant model.",
-#     )
-
-#     data_url: str = pd.Field(
-#         None,
-#         title="Dispersion data URL",
-#         description="The URL to access the dispersion data upon which the material "
-#         "model is fitted.",
-#     )
+new_material_refs = dict(
+    AllResist=ReferenceData(
+        journal="E-Beam Resist AR-P 6200 series (CSAR 62) Product information",
+        url="https://www.allresist.com/wp-content/uploads/sites/2/2020/03/AR-P6200_CSAR62english_Allresist_product-information.pdf",
+    ),
+    Zelmon1997=ReferenceData(
+        journal="D. E. Zelmon, D. L. Small, and D. Jundt. "
+        "Infrared corrected Sellmeier coefficients for congruently grown "
+        "lithium niobate and 5 mol.% magnesium oxide-doped lithium niobate, "
+        "J. Opt. Soc. Am. B 14, 3319-3322 (1997)",
+        doi="https://doi.org/10.1364/JOSAB.14.003319",
+    ),
+)
 
 
 Csar62 = VariantItem(
@@ -88,13 +80,77 @@ Csar62 = VariantItem(
             ),
         ),
         frequency_range=(119964969187675.06, 599584916000000.0)),
+    reference=[new_material_refs["AllResist"]],
     data_url="https://www.allresist.com/wp-content/uploads/sites/2/2020/03/AR-P6200_CSAR62english_Allresist_product-information.pdf",
 )
 
-material_library['Csar62'] = MaterialItem(
-    name="CSAR62 resist",
-    variants=dict(
-        AllResist=Csar62
-    ),
-    default="AllResist",
+
+LiNbO3_o = VariantItem(
+    medium=Sellmeier(
+        coeffs=(
+            (
+                (2.6734),
+                (0.01764)
+            ),
+            (
+                (1.2290),
+                (0.05914)
+            ),
+            (
+                (12.614),
+                (474.60)
+            ),
+        ),
+        frequency_range=(59958491600000.0, 749481145000000.0)
+    ).pole_residue,
+    reference=[new_material_refs["Zelmon1997"]],
+    data_url="https://refractiveindex.info/data_csv.php?datafile=data/main/LiNbO3/Zelmon-o.yml",
 )
+
+
+LiNbO3_e = VariantItem(
+    medium=Sellmeier(
+        coeffs=(
+            (
+                (2.9804),
+                (0.02047)
+            ),
+            (
+                (0.5981),
+                (0.0666)
+            ),
+            (
+                (8.9543),
+                (416.08)
+            ),
+        ),
+        frequency_range=(59958491600000.0, 749481145000000.0),
+    ).pole_residue,
+    reference=[new_material_refs["Zelmon1997"]],
+    data_url="https://refractiveindex.info/data_csv.php?datafile=data/main/LiNbO3/Zelmon-e.yml",
+)
+
+
+new_material_library = dict(
+    Csar62=MaterialItem(
+        name="CSAR62 resist",
+        variants=dict(
+            AllResist=Csar62
+        ),
+        default="AllResist",
+    ),
+    LiNbO3=MaterialItem(
+        name="Lithium Niobate",
+        variants=dict(
+            ordinary=LiNbO3_o,
+            extraordinary=LiNbO3_e,
+        ),
+        default="ordinary"
+    )
+)
+
+
+# PEP 584 add the possibility to merge dict using | operator,
+# but since it is valable for Python>=3.9, use this method:
+material_refs = {**tidy3d_material_refs, **new_material_refs}
+material_library = {**tidy3d_material_library, **new_material_library}
